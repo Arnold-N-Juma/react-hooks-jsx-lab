855608e835f4fde1890e3ec07dfb8edc11b365f5
Arnold Juma <+>
2023-10-26 18:34:43 +0300

    Assignment Submission

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,14 @@
 import React from "react";
 import { name, city } from "../data/data.js";
 
-function Home() {
+export default function Home() {
+  const name = "Liza";
+  const city = "New York";
   // update the JSX being returned!
-  return <div>Home</div>;
+  return <div id="home">
+    <h1>{name} is a Web Developer from {city}</h1>
+
+
+  </div>
 }
 
-export default Home;
